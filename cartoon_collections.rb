c90c1d760f4d2e6769b64ad204ff6b6d8cdbfe9c
Wesley Chen <weschen1996@gmail.com>
2020-01-19 19:35:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,10 @@ def summon_captain_planet(arr)
 end
 
 def long_planeteer_calls(arr)
-  arr.
+  arr.map(|x| if x.length > 4
+                return true
+              end)
+  return false
 end
 
 def find_the_cheese# code an argument here
