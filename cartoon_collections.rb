3f0b7feefacac446fcb9aa10ff495809e42c6d2c
Wesley Chen <weschen1996@gmail.com>
2020-01-19 19:28:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(arr)
-  arr.each_with_index{|x, index| puts index + ". " x}
+  arr.each_with_index{|x, index| puts index + ". "  + x}
 end
 
 def summon_captain_planet# code an argument here
